31a0620a19c34d144f0216464b7ec80bfeabd4c1
Yash <ya17kun@gmail.com>
2023-06-14 22:00:10 +0530

    add project structure

---
+++ src/main/java/com/web/employeeportal/EmployeePortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.web.employeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePortalApiApplication.class, args);
+	}
+
+}
